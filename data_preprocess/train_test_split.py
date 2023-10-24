0a2cb0fa3c910dcba3359e8283b65c232afe473e
ywy <1758863328@qq.com>
2023-10-24 19:52:15 +0800

    处理一期数据

--- data_preprocess/train_test_split.py
+++ data_preprocess/train_test_split.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
-    data=pd.read_excel("../data/上海数据1067+397.xlsx",sheet_name="train")
+    data=pd.read_excel("../data/一期数据.xlsx",sheet_name="train")
     y=data.loc[:,"tumor_nature"]
     X=data.drop(["tumor_nature"],axis=1)
     x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
