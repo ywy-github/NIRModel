bbd58ba59161373e6d809f8c4636ef41b7e18afa
ywy <1758863328@qq.com>
2023-10-24 20:27:51 +0800

    处理二期数据

--- data_preprocess/MovingImages.py
+++ data_preprocess/MovingImages.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 if __name__ == '__main__':
     # 读取Excel文件
-    excel_file = '../data/一期数据.xlsx'
+    excel_file = '../data/二期数据.xlsx'
     df = pd.read_excel(excel_file,sheet_name="test")
 
     # 原始图片文件夹和目标文件夹路径
     source_folder = '../image_all_noclip'
-    output_folder = '../data/一期数据/test'  # 存放所有数据的主文件夹
+    output_folder = '../data/二期数据/test'  # 存放所有数据的主文件夹
 
     # 创建主文件夹
     os.makedirs(output_folder, exist_ok=True)
