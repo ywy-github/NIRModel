0f6864a50e7af7b025ad4fd2348bbe62f82f5707
ywy <1758863328@qq.com>
2023-10-30 10:03:14 +0800

    resnet2

--- Main/ResNet2.py
+++ Main/ResNet2.py
@@ -1,79 +1,14 @@
-from __future__ import print_function
-
 import time
 
-import matplotlib.pyplot as plt
-from six.moves import xrange
-
-import torch.nn as nn
-import torch.nn.functional as F
-from tensorboard import summary
-from torch.utils.data import DataLoader
-import torch.optim as optim
-from torchvision import transforms
 import torch
-import torch.utils.data as data
-
-import numpy as np
-from PIL import Image
-import glob
-import random
-
+from matplotlib import pyplot as plt
+from tensorboard import summary
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import models
+from torchvision import transforms
 from Main.Metrics import all_metrics
 from Main.data_loader import MyData
-
-class Residual(nn.Module):  #@save
-    def __init__(self, input_channels, num_channels,
-                 use_1x1conv=False, strides=1):
-        super().__init__()
-        self.conv1 = nn.Conv2d(input_channels, num_channels,
-                               kernel_size=3, padding=1, stride=strides)
-        self.conv2 = nn.Conv2d(num_channels, num_channels,
-                               kernel_size=3, padding=1)
-        if use_1x1conv:
-            self.conv3 = nn.Conv2d(input_channels, num_channels,
-                                   kernel_size=1, stride=strides)
-        else:
-            self.conv3 = None
-        self.bn1 = nn.BatchNorm2d(num_channels)
-        self.bn2 = nn.BatchNorm2d(num_channels)
-
-    def forward(self, X):
-        Y = F.relu(self.bn1(self.conv1(X)))
-        Y = self.bn2(self.conv2(Y))
-        if self.conv3:
-            X = self.conv3(X)
-        Y += X
-        return F.relu(Y)
-
-def resnet_block(input_channels, num_channels, num_residuals,
-                 first_block=False):
-    blk = []
-    for i in range(num_residuals):
-        if i == 0 and not first_block:
-            blk.append(Residual(input_channels, num_channels,
-                                use_1x1conv=True, strides=2))
-        else:
-            blk.append(Residual(num_channels, num_channels))
-    return blk
-
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.b1=nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
-                         nn.BatchNorm2d(64), nn.ReLU(),
-                         nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-        self.b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True)),
-        self.b3 = nn.Sequential(*resnet_block(64, 128, 2)),
-        self.b4 = nn.Sequential(*resnet_block(128, 256, 2)),
-        self.b5 = nn.Sequential(*resnet_block(256, 512, 2)),
-        self.net = nn.Sequential(*self.b1, *self.b2, *self.b3, *self.b4, *self.b5,
-                            nn.AdaptiveAvgPool2d((1, 1)),
-                            nn.Flatten(), nn.Linear(512, 2))
-
-    def forward(self, x):
-        output = self.net(x)
-        return output
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -88,12 +23,12 @@ if __name__ == '__main__':
         transforms.Normalize((0.3281,), (0.2366,))  # 设置均值和标准差
     ])
 
-    train_benign_data = MyData("../data/train/benign", "benign", transform=transform)
-    train_malignat_data = MyData("../data/train/malignant", "malignant", transform=transform)
+    train_benign_data = MyData("../data/一期数据/train/benign", "benign", transform=transform)
+    train_malignat_data = MyData("../data/一期数据/train/malignant", "malignant", transform=transform)
     train_data = train_benign_data + train_malignat_data
 
-    val_benign_data = MyData("../data/val/benign", "benign", transform=transform)
-    val_malignat_data = MyData("../data/val/malignant", "malignant", transform=transform)
+    val_benign_data = MyData("../data/一期数据/val/benign", "benign", transform=transform)
+    val_malignat_data = MyData("../data/一期数据/val/malignant", "malignant", transform=transform)
     val_data = val_benign_data + val_malignat_data
 
     training_loader = DataLoader(train_data,
@@ -108,7 +43,13 @@ if __name__ == '__main__':
 
 
 
-    model = Model().to(device)
+    model = models.resnet18(pretrained=False)
+
+    #调整结构
+    model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    num_classes = 1
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    model.add_module("sigmoid", nn.Sigmoid())
 
     criterion = nn.CrossEntropyLoss()
     criterion.to(device)
@@ -129,15 +70,14 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             output = model(images)
             loss = criterion(output,targets)
+            total_train_loss+=loss.item()
             loss.backward()
             optimizer.step()
 
-            total_train_loss += loss.item()
-            _, predicted = torch.max(output, 1)
-            train_predictions.extend(predicted.cpu().numpy())
+            predicted_labels = (output >= 0.5).int().squeeze()
+            train_predictions.extend(predicted_labels.cpu().numpy())
             train_targets.extend(targets.cpu().numpy())
-        if((epoch+1)%10==0):
-            train_losses.append(loss.item())
+            total_train_loss.append(total_train_loss.item())
 
         model.eval()
         val_predictions = []
@@ -145,16 +85,19 @@ if __name__ == '__main__':
         total_val_loss = 0
         with torch.no_grad():
             for batch in validation_loader:
-                images, targets = batch
+                images, targets, names = batch
                 images = images.to(device)
                 targets = targets.to(device)
                 output = model(images)
                 loss = criterion(output, targets)
                 total_val_loss += loss.item()
-                _, predicted = torch.max(output, 1)
-                val_predictions.extend(predicted.cpu().numpy())
+
+                predicted_labels = (output >= 0.5).int().squeeze()
+                val_predictions.extend(predicted_labels.cpu().numpy())
                 val_targets.extend(targets.cpu().numpy())
-        if ((epoch + 1) % 10 == 0):
+                validation_losses.append(total_val_loss.item())
+
+        if ((epoch + 1) % 1 == 0):
             # torch.save(models, "VQ_VAE{}.pth".format(i+1))
             print('%d epoch' % (epoch + 1))
 
