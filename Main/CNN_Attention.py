74214a3403966186df75163e159d8a64a3ab635a
ywy <1758863328@qq.com>
2023-11-06 14:45:02 +0800

    VQ-VAE-resnet18_data2_test1

--- Main/CNN_Attention.py
+++ Main/CNN_Attention.py
@@ -12,54 +12,7 @@ from torchvision import models
 from torchvision import transforms
 from Main.Metrics import all_metrics
 from Main.data_loader import MyData
-
-#通道注意力模块
-class ChannelAttention(nn.Module):
-    def __init__(self, in_planes, ratio=16):
-        super(ChannelAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-
-        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
-
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
-        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
-        out = avg_out + max_out
-        return self.sigmoid(out)
-
-#空间注意力模块
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
-
-        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
-        padding = 3 if kernel_size == 7 else 1
-
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
-        return self.sigmoid(x)
-
-#先接通道注意力模块、再接空间注意力模块
-class CBAM(nn.Module):
-    def __init__(self, inplanes, ratio=16, kernel_size=3):
-        super(CBAM, self).__init__()
-        self.ca = ChannelAttention(inplanes, ratio)
-        self.sa = SpatialAttention(kernel_size)
-    def forward(self, x):
-        out = x*self.ca(x)
-        result = out*self.sa(out)
-        return result
+from Attention_block import CBAM
 
 
 # 定义自定义损失函数，加权二进制交叉熵
@@ -190,7 +143,7 @@ if __name__ == '__main__':
         writer.add_scalar('Loss/Val', total_val_loss, epoch)
 
         if ((epoch + 1) % 50 == 0):
-            torch.save(model, "../models/resnet/resnet18{}.pth".format(epoch+1))
+            torch.save(model, "../models/Attention/attention{}.pth".format(epoch+1))
             print('%d epoch' % (epoch + 1))
 
             train_acc, train_sen, train_spe = all_metrics(train_targets, train_pred)
