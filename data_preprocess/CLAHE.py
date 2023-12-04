ad42593772091bcf1bbeb2c2ad8ebde43c97a93f
ywy <1758863328@qq.com>
2023-12-04 17:13:38 +0800

    update

--- data_preprocess/CLAHE.py
+++ data_preprocess/CLAHE.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 from PIL import Image
 
-def apply_clahe(image, clip_limit=3.0, grid_size=(8, 8)):
+def apply_clahe(image, clip_limit=2.0, grid_size=(10, 10)):
     if image.mode == 'L':
         gray_image = np.array(image)
     else:
