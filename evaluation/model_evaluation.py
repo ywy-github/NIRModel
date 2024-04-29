541b707bef5f52fcccf66b5b748e2469ca2282aa
ywy <1758863328@qq.com>
2024-04-29 18:57:13 +0800

    update

--- evaluation/model_evaluation.py
+++ evaluation/model_evaluation.py
@@ -18,7 +18,75 @@ def ROC_Curve(y_true,y_pred):
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
+    plt.title('Receiver Operating Characteristic Curve')
+    plt.legend(loc="lower right")
+    plt.show()
+
+def Multi_ROC_Curve():
+    filepath = "../models2/excels"
+    data_AlexNet = pd.read_excel(filepath + "/AlexNet-5.xlsx")
+    data_DenseNet = pd.read_excel(filepath + "/DenseNet-13.xlsx")
+    data_MobileNet = pd.read_excel(filepath + "/MobileNet-13.xlsx")
+    data_Resnet18 = pd.read_excel(filepath + "/Resnet18-8.xlsx")
+    data_Resnet50 = pd.read_excel(filepath + "/Resnet50-16.xlsx")
+    data_RCNet = pd.read_excel(filepath + "/Vq-VAE-resnet18仅重构+分类器-71.xlsx")
+    data_SRCNet = pd.read_excel(filepath + "/VQ-VAE-resnet18-29.xlsx")
+
+    y_true_AlexNet = data_AlexNet.loc[:, "label"]
+    y_true_DenseNet = data_DenseNet.loc[:, "label"]
+    y_true_MobileNet = data_MobileNet.loc[:, "label"]
+    y_true_Resnet18 = data_Resnet18.loc[:, "label"]
+    y_true_Resnet50 = data_Resnet50.loc[:, "label"]
+    y_true_RCNet = data_RCNet.loc[:, "label"]
+    y_true_SRCNet = data_SRCNet.loc[:, "label"]
+
+    y_scores_AlexNet = data_AlexNet.loc[:, "pred"]
+    y_scores_DenseNet = data_DenseNet.loc[:, "pred"]
+    y_scores_MobileNet = data_MobileNet.loc[:, "pred"]
+    y_scores_Resnet18 = data_Resnet18.loc[:, "pred"]
+    y_scores_Resnet50 = data_Resnet50.loc[:, "pred"]
+    y_scores_RCNet = data_RCNet.loc[:, "pred"]
+    y_scores_SRCNet = data_SRCNet.loc[:, "pred"]
+
+    fpr_AlexNet, tpr_AlexNet, thresholds_AlexNet = roc_curve(y_true_AlexNet, y_scores_AlexNet)
+    fpr_DenseNet, tpr_DenseNet, thresholds_DenseNet = roc_curve(y_true_DenseNet,y_scores_DenseNet)
+    fpr_MobileNet, tpr_MobileNet, thresholds_MobileNet = roc_curve(y_true_MobileNet,y_scores_MobileNet)
+    fpr_Resnet18, tpr_Resnet18, thresholds_Resnet18 = roc_curve(y_true_Resnet18,y_scores_Resnet18)
+    fpr_Resnet50, tpr_Resnet50, thresholds_Resnet50 = roc_curve(y_true_Resnet50,y_scores_Resnet50)
+    fpr_RCNet, tpr_RCNet, thresholds_RCNet = roc_curve(y_true_RCNet, y_scores_RCNet)
+    fpr_SRCNet, tpr_SRCNet, thresholds_SRCNet = roc_curve(y_true_SRCNet, y_scores_SRCNet)
+
+    roc_auc_AlexNet = auc(fpr_AlexNet, tpr_AlexNet)
+    roc_auc_DenseNet = auc(fpr_DenseNet, tpr_DenseNet)
+    roc_auc_MobileNet = auc(fpr_MobileNet, tpr_MobileNet)
+    roc_auc_Resnet18 = auc(fpr_Resnet18, tpr_Resnet18)
+    roc_auc_Resnet50 = auc(fpr_Resnet50, tpr_Resnet50)
+    roc_auc_RCNet = auc(fpr_RCNet, tpr_RCNet)
+    roc_auc_SRCNet = auc(fpr_SRCNet, tpr_SRCNet)
+
+    plt.figure()
+
+    plt.plot(fpr_Resnet18, tpr_Resnet18, linewidth=2,
+             label='Resnet18 (area = %0.4f)' % roc_auc_Resnet18)
+    plt.plot(fpr_Resnet50, tpr_Resnet50, linewidth=2,
+             label='Resnet50 (area = %0.4f)' % roc_auc_Resnet50)
+    plt.plot(fpr_AlexNet, tpr_AlexNet, linewidth=2,
+             label='AlexNet (area = %0.4f)' % roc_auc_AlexNet)
+    plt.plot(fpr_MobileNet, tpr_MobileNet, linewidth=2,
+             label='MobileNet (area = %0.4f)' % roc_auc_MobileNet)
+    plt.plot(fpr_DenseNet, tpr_DenseNet, linewidth=2,
+             label='DenseNet (area = %0.4f)' % roc_auc_DenseNet)
+    plt.plot(fpr_RCNet, tpr_RCNet, linewidth=2,
+             label='RCNet (area = %0.4f)' % roc_auc_RCNet)
+    plt.plot(fpr_SRCNet, tpr_SRCNet, linewidth=2,
+             label='SRCNet (area = %0.4f)' % roc_auc_SRCNet)
+
+    plt.plot([0, 1], [0, 1], color='navy', linewidth=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic Curve')
     plt.legend(loc="lower right")
     plt.show()
 
@@ -34,6 +102,58 @@ def PR_Curve(y_true,y_pred):
     plt.ylabel('precision')
     plt.title('precision-recall curve')
     plt.show()
+
+def Multi_PR_Curve():
+    filepath = "../models2/excels"
+    data_AlexNet = pd.read_excel(filepath + "/AlexNet-5.xlsx")
+    data_DenseNet = pd.read_excel(filepath + "/DenseNet-13.xlsx")
+    data_MobileNet = pd.read_excel(filepath + "/MobileNet-13.xlsx")
+    data_Resnet18 = pd.read_excel(filepath + "/Resnet18-8.xlsx")
+    data_Resnet50 = pd.read_excel(filepath + "/Resnet50-16.xlsx")
+    data_RCNet = pd.read_excel(filepath + "/Vq-VAE-resnet18仅重构+分类器-71.xlsx")
+    data_SRCNet = pd.read_excel(filepath + "/VQ-VAE-resnet18-29.xlsx")
+
+    y_true_AlexNet = data_AlexNet.loc[:, "label"]
+    y_true_DenseNet = data_DenseNet.loc[:, "label"]
+    y_true_MobileNet = data_MobileNet.loc[:, "label"]
+    y_true_Resnet18 = data_Resnet18.loc[:, "label"]
+    y_true_Resnet50 = data_Resnet50.loc[:, "label"]
+    y_true_RCNet = data_RCNet.loc[:, "label"]
+    y_true_SRCNet = data_SRCNet.loc[:, "label"]
+
+    y_scores_AlexNet = data_AlexNet.loc[:, "pred"]
+    y_scores_DenseNet = data_DenseNet.loc[:, "pred"]
+    y_scores_MobileNet = data_MobileNet.loc[:, "pred"]
+    y_scores_Resnet18 = data_Resnet18.loc[:, "pred"]
+    y_scores_Resnet50 = data_Resnet50.loc[:, "pred"]
+    y_scores_RCNet = data_RCNet.loc[:, "pred"]
+    y_scores_SRCNet = data_SRCNet.loc[:, "pred"]
+
+    precision_AlexNet, recall_AlexNet, thresholds_AlexNet = precision_recall_curve(y_true_AlexNet, y_scores_AlexNet)
+    precision_DenseNet, recall_DenseNet, thresholds_DenseNet = precision_recall_curve(y_true_DenseNet, y_scores_DenseNet)
+    precision_MobileNet, recall_MobileNet, thresholds_MobileNet = precision_recall_curve(y_true_MobileNet, y_scores_MobileNet)
+    precision_Resnet18, recall_Resnet18, thresholds_Resnet18 = precision_recall_curve(y_true_Resnet18, y_scores_Resnet18)
+    precision_Resnet50, recall_Resnet50, thresholds_Resnet50 = precision_recall_curve(y_true_Resnet50, y_scores_Resnet50)
+    precision_RCNet, recall_RCNet, thresholds_RCNet = precision_recall_curve(y_true_RCNet, y_scores_RCNet)
+    precision_SRCNet, recall_SRCNet, thresholds_SRCNet = precision_recall_curve(y_true_SRCNet, y_scores_SRCNet)
+
+    plt.plot(precision_Resnet18, recall_Resnet18, label="Resnet18")
+    plt.plot(precision_Resnet50, recall_Resnet50, label="Resnet50")
+    plt.plot(precision_AlexNet, recall_AlexNet, label="AlexNet")
+    plt.plot(precision_MobileNet, recall_MobileNet, label="MobileNet")
+    plt.plot(precision_DenseNet, recall_DenseNet, label="DenseNet")
+    plt.plot(precision_RCNet, recall_RCNet, label="RCNet")
+    plt.plot(precision_SRCNet, recall_SRCNet, label="SRCNet")
+
+    plt.legend(loc="upper right")
+    plt.xlim([0.0, 1])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.title('precision-recall curve')
+
+    plt.show()
+
 
 
 # y_true:真实标签
@@ -93,13 +213,14 @@ def compute(y_true,y_prob,y_pred):
 
 
 if __name__ == '__main__':
-    data = pd.read_excel("../models1/result/resnet18+删一个layer.xlsx")
+    data = pd.read_excel("../models2/excels/AlexNet-5.xlsx")
     y_true = data.loc[:,"label"]
     y_prob = data.loc[:,"prob"]
     y_pred = data.loc[:,"pred"]
     # plot_confusion_matrix(y_true,y_prob)
-    #
     # PR_Curve(y_true,y_pred)
     # ROC_Curve(y_true,y_pred)
 
     compute(y_true,y_prob,y_pred)
+    # Multi_PR_Curve()
+    # Multi_ROC_Curve()
